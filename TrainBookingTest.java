835b3553f16008b3f9fa03b114f638787b806957
Rohit Verma <30729211+Vermarohit1151@users.noreply.github.com>
2022-05-31 14:18:01 +0530

    Add files via upload

---
+++ TrainBookingTest.java
@@ -0,0 +1,49 @@
+import java.time.LocalDate;
+import java.util.Random;
+public class TrainBookingTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ticket passenger1 = new Ticket();
+		
+		passenger1.bookTicket("Rohit Verma","KYN","CSMT",2022,05,31);
+		passenger1.printTicket();
+		System.out.println();
+		passenger1.cancelTicket(7000647);
+	}
+
+}
+
+class Ticket{
+	
+	int PNR;
+	String passengerName, Src, Dest;
+	LocalDate dateOfJourney;
+	int ticketPrice ;
+	
+	Random random;
+	void bookTicket(String PName,String Source, String Destin,int Year,int Month,int day) {
+		random  = new Random();
+		this.PNR = random.nextInt(1154244, 9912345);
+		this.ticketPrice=random.nextInt(200, 1000);
+		
+		passengerName = PName;
+		Src = Source;
+		Dest = Destin;
+		dateOfJourney = LocalDate.of(Year, Month, day);
+	}
+	
+	void printTicket() {
+		System.out.println("PNR :"+PNR);
+		System.out.println("NAME :"+passengerName);
+		System.out.println("SOURCE :"+Src +"\tDESTINATION :"+Dest);
+		System.out.println("DOJ: "+dateOfJourney);
+		System.out.println("COST: Rs."+ticketPrice );
+	
+	}
+	
+	void cancelTicket(int PNR) {
+		System.out.println("Your Ticket Number "+PNR+" is cancelled succesfully");
+	}
+	
+}
