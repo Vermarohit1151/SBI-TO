c13e411075f06aba079e7b625437d9dbc385d0e5
Rohit Verma <30729211+Vermarohit1151@users.noreply.github.com>
2022-06-01 15:09:34 +0530

    Add files via upload

--- PersonTest.java
+++ PersonTest.java
@@ -1,8 +1,12 @@
 
 public class PersonTest {
 public static void main(String[] args) {
-	Employee Emp1 = new Employee('M',23,"Rohit Verma",42,"LTCE","CS",1031267,"SBI","Assistant Manager",50000);
-	System.out.println(Emp1);
+	//Employee Emp1 = new Employee('M',23,"Rohit Verma",42,"LTCE","CS",1031267,"SBI","Assistant Manager",50000);
+	//System.out.println(Emp1);
+	
+	Student p1 = new Student('M', 23, "Rohit Verma", 42,"LTCE","CS","DY Patil Marg", "EA 1/7", "Nerul", "MH", "INDIA",400614,
+			"Subhash nagar","702","Kalyan","MH","INDIA",421301);
+	p1.printPerson();
 }
 }
 
@@ -10,17 +14,37 @@ class Person{
 	private char gender;
 	private int age;
 	private String name;
-	
+	Address homeAddress;
+	Address corrAddress;
 	public Person(char gender, int age, String name) {
 		super();
 		this.gender = gender;
 		this.age = age;
 		this.name = name;
 	}
-
-	@Override
-	public String toString() {
-		return "\nPerson [gender=" + gender + ", age=" + age + ", name=" + name + "]";
+	
+	public Person(char gender, int age, String name, String area, String house, String city, String state, String country, int pin,
+			String area1, String house1, String state1, String city1, String country1, int pin1) {
+		super();
+		this.gender = gender;
+		this.age = age;
+		this.name = name;
+		this.homeAddress = new Address(area,house,city,state,country,pin);
+		this.corrAddress = new Address(area1, house1, city1, state1, country1, pin1);
+	}
+	
+	public void printPerson() {
+		System.out.println("NAME   	 : "+name);
+		System.out.println("Age    	 :" +age);
+		System.out.println("GENDER 	 : "+gender);
+		if(homeAddress != null) {
+			System.out.println("\nHOME ADDRESS");
+			homeAddress.printAddress();
+		}
+		if(corrAddress!=null) {
+			System.out.println("\nCORRESEPONDENCE ADDRESS");
+			corrAddress.printAddress();
+		}
 	}
 	
 	
@@ -35,6 +59,18 @@ class Student extends Person{
 		this.rollno = rollno;
 		this.collegeName = collegeName;
 		this.stream = stream;
+	}
+	
+	public Student(char gender, int age, String name,int rollno, String collegeName, String stream, String area, String house, String city, String state,
+			String country, int pin, String area1, String house1, String state1, String city1, String country1,
+			int pin1) 
+	{
+		super(gender, age, name, area, house, city, state, country, pin, area1, house1, state1, city1, country1, pin1);
+		this.rollno = rollno;
+		this.collegeName = collegeName;
+		this.stream = stream;
+		
+		// TODO Auto-generated constructor stub
 	}
 
 	@Override
@@ -66,5 +102,79 @@ class Employee extends Student{
 				+ ", salary=" + salary + "]";
 	}
 	
+	
+}
+
+class Address{
+	private String Area,House,City,State,Country;
+	private int pin;
+	
+	public Address(String area, String house, String city, String state, String country, int pin) {
+		super();
+		Area = area;
+		House = house;
+		City = city;
+		State = state;
+		Country = country;
+		this.pin = pin;
+	}
+
+	public String getArea() {
+		return Area;
+	}
+
+	public void setArea(String area) {
+		Area = area;
+	}
+
+	public String getHouse() {
+		return House;
+	}
+
+	public void setHouse(String house) {
+		House = house;
+	}
+
+	public String getCity() {
+		return City;
+	}
+
+	public void setCity(String city) {
+		City = city;
+	}
+
+	public String getState() {
+		return State;
+	}
+
+	public void setState(String state) {
+		State = state;
+	}
+
+	public String getCountry() {
+		return Country;
+	}
+
+	public void setCountry(String country) {
+		Country = country;
+	}
+
+	public int getPin() {
+		return pin;
+	}
+
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+	
+	
+    public void printAddress() {
+    	System.out.println("FLAT NO       :"+House);
+    	System.out.println("AREA          :"+Area);
+    	System.out.println("CITY          :"+City);
+    	System.out.println("STATE         :"+State);
+    	System.out.println("COUNTRY       :"+Country);
+    	System.out.println("PINCODE       :"+pin);
+    }
 	
 }
