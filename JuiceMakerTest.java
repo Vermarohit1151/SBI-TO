41771458cb0bbd3419e810d2d0cff5371f849ba6
Rohit Verma <30729211+Vermarohit1151@users.noreply.github.com>
2022-06-03 09:25:11 +0530

    Add files via upload

--- JuiceMakerTest.java
+++ JuiceMakerTest.java
@@ -3,27 +3,28 @@ public class JuiceMakerTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Fruits fruit = new Fruits("Apple",4);
+		Fruit newFruit ;
+		
+		newFruit = new Mango("");
 		Ingredients ingredients = new Ingredients(0.5f,10,1.2f);
 		JuiceMaker jMaker = new JuiceMaker();
-		
-		jMaker.makeJuice(fruit, ingredients);
+		jMaker.makeJuice(newFruit, ingredients);
 	}
 
 }
 
-class machine{
+abstract class machine{
 	int serialNumber;
 	LocalDate mfgDate;
 	String countryOfOrigin;
-	
+
 }
 
 class Jar {
-	 int capacity;
-	 int noOfblades;
-	 String material;
-	 
+	int capacity;
+	int noOfblades;
+	String material;
+
 	public Jar(int capacity, int noOfblades, String material) {
 		super();
 		this.capacity = capacity;
@@ -32,56 +33,124 @@ class Jar {
 	}
 }
 
-class Juice{
-	
-	Juice(Fruits fruitObj)
-	{
-		System.out.println(fruitObj.fruitName+" Juice is ready to consume...");
-	}
+abstract class Juice{
+	abstract void taste();
+	//System.out.println(fruitObj.fruitName+" Juice is ready to consume...");
+	abstract void Juice(Fruit f);
 }
 
-class Fruits{
-	String fruitName;
-	int qty;
-	
-	public Fruits(String fruitName, int qty) {
-		super();
-		this.fruitName = fruitName;
-		this.qty = qty;
+class MangoJuice extends Juice{
+
+	@Override
+	void taste() {
+		System.out.println("Its a Sweet juice...");
+
 	}
+
+	@Override
+	void Juice(Fruit f) {
+		System.out.println("Your "+f.fruitName+" Juice is ready to consume");
+
+	}
+
+}	
+class OrangeJuice extends Juice{
+
+	@Override
+	void taste() {
+		System.out.println("Its a Sour juice...");
+
+	}
+
+	@Override
+	void Juice(Fruit f) {
+		System.out.println("Your "+f.fruitName+" Juice is ready to consume");
+
+	}
+
+}
+
+
+
+
+
+
+abstract class Fruit{
+	String fruitName;
+	int Qty;
+}
+
+
+
+class Orange extends Fruit{
+ public Orange(String f) {
+	 System.out.println("A new Fruit Object ORANGE is created");
+ }
+}
+
+class Mango extends Fruit{
 	
+	public Mango(Fruit f){
+		 System.out.println("A new Fruit Object MANGO is created");
+	}
 }
 
 class Ingredients{
 	float qtySugar;
 	int qtyIceCubes;
 	float qtyMilk;
-	
+
 	public Ingredients(float qtySugar, int qtyIceCubes, float qtyMilk) {
 		super();
 		this.qtySugar = qtySugar;
 		this.qtyIceCubes = qtyIceCubes;
 		this.qtyMilk = qtyMilk;
 	}
-	
+
 }
 
-class JuiceMaker extends machine{   				 //Is-A relationship
+interface Mixer{
+	void mixing();
+}
+
+interface Grinder{
+	void grinding();
+}
+
+class JuiceMaker extends machine implements Mixer,Grinder
+{   				 //Is-A relationship
 
 	String brand;
 	String material;
 	String type;
 	int power;
 	int Price;
-	
-	
+
+
 	Jar juiceJar = new Jar(1,3,"ABS Plastic");   	  //Has-A relationship
+
 	
-	
-	public Juice makeJuice(Fruits fruit,Ingredients ingredients) {    //Uses-A
-		Juice juice = new Juice(fruit);
-		return juice;												 //Produces-A
+	public Juice makeJuice(Fruit Mango,Ingredients ingredients) {    //Uses-A
+		Juice j = new MangoJuice();
+		j.taste();
+		j.Juice(Mango);
+		JuiceMaker jj = new JuiceMaker();
+		jj.grinding();
+		jj.mixing();
+		return j;												 //Produces-A
+	}
+
+
+	@Override
+	public void grinding() {
+		System.out.println("Grinding the fruits...");
+
+	}
+
+
+	@Override
+	public void mixing() {
+		System.out.println("Mixing the fruit juice...");
+
 	}
 }
-
- 
