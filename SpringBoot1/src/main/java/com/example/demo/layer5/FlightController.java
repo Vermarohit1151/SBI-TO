198eaa6258d2ac8a19d3206390466995a0dfcdf0
Rohit Verma <30729211+Vermarohit1151@users.noreply.github.com>
2022-06-22 18:12:02 +0530

    Add files via upload

---
+++ SpringBoot1/src/main/java/com/example/demo/layer5/FlightController.java
@@ -0,0 +1,73 @@
+package com.example.demo.layer5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.ResponseStatus;
+import com.example.demo.exceptions.FlightListEmptyException;
+import com.example.demo.exceptions.FlightNotFoundException;
+import com.example.demo.layer2.Flight;
+import com.example.demo.layer4.FlightService;
+
+
+// http://localhost:8080
+//					|
+// http://localhost:4200
+
+@CrossOrigin			//CORS
+@RestController
+@RequestMapping("/flight")
+public class FlightController {
+
+	@Autowired
+	FlightService flightService;
+//import org.springframework.http.ResponseEntity;
+	@RequestMapping("/getFlight/{fid}")
+	public ResponseEntity<Flight> getFlightObject(@PathVariable("fid") int flightNumber) throws FlightNotFoundException 
+	{
+		Flight flight = null;
+		ResponseEntity responseRef=null;
+		try {
+			flight = flightService.findFlightByIdService(flightNumber);
+			responseRef = ResponseEntity.ok(flight);
+			return responseRef;
+		} catch (FlightNotFoundException e) {
+			throw e;
+			//System.out.println("Inside flight not found exception ");
+			//ResponseStatus respStatus = new ResponseStatus();
+			//respStatus.setMessage(e.getMessage());
+			//responseRef = ResponseEntity.status(HttpStatus.NOT_FOUND).body(respStatus);
+			//return ;
+		}
+		//return responseRef;
+	}
+	
+	
+	@RequestMapping("/getFlights")
+	public ResponseEntity getFlights() throws FlightListEmptyException 
+	{
+		List<Flight> flightList = new ArrayList<Flight>();
+		ResponseEntity responseRef=null;
+		try {
+			flightList = flightService.listAllFlightService();
+			responseRef = ResponseEntity.ok(flightList);
+			return responseRef;
+		} catch (FlightListEmptyException e) {
+			throw e;
+			//System.out.println("Inside flight not found exception ");
+			//ResponseStatus respStatus = new ResponseStatus();
+			//respStatus.setMessage(e.getMessage());
+			//responseRef = ResponseEntity.status(HttpStatus.NOT_FOUND).body(respStatus);
+			//return ;
+		}
+		//return responseRef;
+	}
+}
